1a905c42be722deddf0558d424b0d1a1a5fc0265
unknown <emiliorodriguezrast@gmail.com>
2024-01-02 18:58:57 -0300

    Me falto guardar los cambios en la barra de nav

---
+++ src/componentes/Barranav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo  from '../assets/logoempresa.png'
+
+function Barranav() {
+  return (
+    <div className='Barranav'>
+        <div className='Izquierda'>
+            <img src={logo}/>
+       </div>
+       <div className='Derecha'>
+       </div>
+       
+    </div>
+  )
+}
+
+export default Barranav
