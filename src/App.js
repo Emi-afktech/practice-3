572a2fa8832babfbab804ea07b3f591795e185a1
unknown <emiliorodriguezrast@gmail.com>
2024-01-04 01:53:05 -0300

    Bibliotecas que permiten el direccionamiento en react y la creación de la página inicial 'aun sin finalizar'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import Barranav from "./components/Barranav";
+import Inicio from "./pages/Inicio";
 import "./App.css";
+import {BrowserRouter as ruta, Route, Switch, Router} from "react-router-dom"
 
 function App() {
-  return <div className="
-  App"></div>;
+  return (
+  <div className="App">
+    //Router es lo que permite tener varias direcciónes dentro de la página
+    <Router> 
+      //Al estar Barranav fuera de los Switches, se importa a todas las direcciónes que ingrese.
+      <Barranav/>
+      //Switch es para diferenciar entre página y página
+      <Switch>
+        <Route path="/" exact component={Inicio}/>
+      </Switch>
+    </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Barranav.js
+++ src/components/Barranav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo  from '../assets/logoempresa.png'
+import { Link } from "react-router-dom"
 
 function Barranav() {
   return (
